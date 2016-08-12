ad6a59db14b0b9e98fd245bed92f608ae2dd94f1
jinmylopez <jinmy.osiris@gmail.com>
2016-08-12 02:59:40 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,6 +5,12 @@
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
     $('.carousel').carousel();
+    
+    linkInterno.on('click',function(e) {
+      e.preventDefault();
+      var href = $(this).attr('href');
+      $('html, body').animate({ scrollTop : $( href ).offset().top }, 'slow', 'easeInOutExpo');
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
